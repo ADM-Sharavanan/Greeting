e74569fd68dbe2616e0449e635899ac401b2fc19
SharavananDemo <ishasharavanan@gmail.com>
2020-07-17 16:07:19 +0530

    commit 1

---
+++ src/study_purpose/Hi_porject.java
@@ -0,0 +1,12 @@
+package study_purpose;
+
+public class Hi_porject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi project");
+
+	}
+
+}

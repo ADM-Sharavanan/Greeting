b02a0e2d4b434a2212399f1a5d95742159bb1dd5
SharavananDemo <ishasharavanan@gmail.com>
2020-07-18 01:26:31 +0530

    Good night file

---
+++ src/study_purpose/Good_Night.java
@@ -0,0 +1,12 @@
+package study_purpose;
+
+public class Good_Night {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Good Night");
+	}
+}

a995c1e4498b7365dade00ef7d7ee327280d5630
SharavananDemo <ishasharavanan@gmail.com>
2020-07-18 13:10:55 +0530

    Good morning code

---
+++ src/study_purpose/Good_morning.java
@@ -0,0 +1,12 @@
+package study_purpose;
+
+public class Good_morning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Good morning");
+	}
+
+}

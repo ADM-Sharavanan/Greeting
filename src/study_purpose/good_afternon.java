bda999ee03b6f72b5d6b44d663811786389784f6
SharavananDemo <ishasharavanan@gmail.com>
2020-07-18 15:05:26 +0530

    Good afternoon

---
+++ src/study_purpose/good_afternon.java
@@ -0,0 +1,12 @@
+package study_purpose;
+
+public class good_afternon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Good afternoon");
+
+	}
+
+}
